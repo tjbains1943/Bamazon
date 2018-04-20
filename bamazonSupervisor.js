a61e657e2e32b8f003d80b7c3b8b1e0f750e79b7
tj Bains <tjbains1943@yahoo.com>
2018-04-19 17:41:06 -0700

    halfway done

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -58,41 +58,6 @@ function viewSales(){
 	})
 };
 
-function newDepart(){
-
-	//creates the questions to be prompted to the user when option 2 is selected - since the department id# auto increments the user doesn't have to enter anything for item id and since total sales is calculated based on sales made  the user doesn't input any data for total sales
-	var newDepartment = {
-		properties: {
-			newDeptName:{ description: colors.magenta('Please enter the name of the new department you would like to add.')
-			},
-			newOverhead:{ description: colors.magenta('What are the overhead costs for this department?')
-			},
-		}
-	}
-
-	prompt.start();
-	//gets the information the user entered for the prompt above
-	prompt.get(newDepartment, function(err, res){
-
-		//creates a variable to store the user responses
-		var newDeptInfo = {
-			deptName: res.newDeptName,
-			overHeadNew: res.newOverhead,
-			autoTotalSales: 0,
-		};
-		//pushes user responses to the array defined above by the variable newDept
-		newDept.push(newDeptInfo);
-		//connects to the mysql database Departments and inserts the information received from the user into the database to create a new department
-		connection.query('INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (?, ?, ?);', [newDept[0].deptName, newDept[0].overHeadNew, newDept[0].autoTotalSales], function(err, result){
-			if(err){
-				console.log('Error: ' + err);
-				connection.end();
-			} else {
-				console.log('');
-				console.log('New Department sucessfully created!');
-				console.log(' ');
-				connection.end();
-			}
-		})
-	})
-};
+function newDepart() {
+    console.log("f");
+}
